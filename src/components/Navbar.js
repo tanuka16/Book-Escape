2d803f0bb253d1d703265553477d358bad406ee0
Tanuka16 <tanuka.das12@gmail.com>
2020-10-03 15:43:02 -0400

    completed the routes and added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import logo1 from '../logo1.png'
 
 class Navbar extends Component {
 
   render() {
     return (
-      <div>
-        <h3> Hey from Navbar</h3>
-      </div>
+      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+
+      <Link to="/">
+        <img src={logo1} alt='store' className='navbar-brand' />
+      </Link>
+        <ul></ul>
+      </nav>
     );
   }
 
