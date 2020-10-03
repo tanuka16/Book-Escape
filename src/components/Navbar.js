cfa2e84c68bcae400a46a5f916abfef009bb84a1
Tanuka16 <tanuka.das12@gmail.com>
2020-10-03 17:13:32 -0400

    completed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,19 @@ class Navbar extends Component {
       <Link to="/">
         <img src={logo1} alt='store' className='navbar-brand' />
       </Link>
-        <ul></ul>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+            Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+        <button>
+          <i className="fas fa-cart-plus" /> My Cart
+        </button>
+
+        </Link>
       </nav>
     );
   }
