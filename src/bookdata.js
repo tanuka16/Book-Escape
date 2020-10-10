ad9e435497458a54383dd0b699cd17a16f280baf
Tanuka16 <tanuka.das12@gmail.com>
2020-10-10 13:58:38 -0400

    added book data

---
+++ src/bookdata.js
@@ -0,0 +1,166 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Fangirl",
+    img: "img/product-1.png",
+    price: 10,
+    author: "Rainbow Rowell",
+    info:
+      "In Rainbow Rowell's Fangirl, Cath is a Simon Snow fan. Okay, the whole world is a Simon Snow fan, but for Cath, being a fan is her life-and she's really good at it. She and her twin sister, Wren, ensconced themselves in the Simon Snow series when they were just kids; it's what got them through their mother leaving.
+
+      Reading. Rereading. Hanging out in Simon Snow forums, writing Simon Snow fan fiction, dressing up like the characters for every movie premiere.
+
+      Cath's sister has mostly grown away from fandom, but Cath can't let go. She doesn't want to.
+
+      Now that they're going to college, Wren has told Cath she doesn't want to be roommates. Cath is on her own, completely outside of her comfort zone. She's got a surly roommate with a charming, always-around boyfriend, a fiction-writing professor who thinks fan fiction is the end of the civilized world, a handsome classmate who only wants to talk about words . . . And she can't stop worrying about her dad, who's loving and fragile and has never really been alone.
+
+      For Cath, the question is: Can she do this? Can she make it without Wren holding her hand? Is she ready to start living her own life? And does she even want to move on if it means leaving Simon Snow behind?
+
+      ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "The Complete Software Developer's Career Guide",
+    img: "img/product-2.png",
+    price: 26.01,
+    author: "John Sonmez",
+    info:
+      "Early in his software developer career, John Sonmez discovered that technical knowledge alone isn't enough to break through to the next income level - developers need "soft skills" like the ability to learn new technologies just in time, communicate clearly with management and consulting clients, negotiate a fair hourly rate, and unite teammates and coworkers in working toward a common goal.
+
+      As John invested in these skills his career took off, and he became a highly paid, highly sought-after developer and consultant.
+
+      Today John helps more than 1.4 million programmers every year to increase their income by developing this unique blend of skills.
+
+      'If you're a developer, green or a veteran, you owe it to yourself to read The Complete Software Developers Career Guide.' - Jason Down, Platform Developer, Ontario, Canada",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Clean Code: A Handbook of Agile Software Craftsmanship",
+    img: "img/product-3.png",
+    price: 44.99,
+    author: "htc",
+    info:
+      "Clean Code is divided into three parts. The first describes the principles, patterns, and practices of writing clean code. The second part consists of several case studies of increasing complexity. Each case study is an exercise in cleaning up code—of transforming a code base that has some problems into one that is sound and efficient. The third part is the payoff: a single chapter containing a list of heuristics and “smells” gathered while creating the case studies. The result is a knowledge base that describes the way we think when we write, read, and clean code.
+
+        Readers will come away from this book understanding
+        How to tell the difference between good and bad code
+        How to write good code and how to transform bad code into good code
+        How to create good names, good functions, good objects, and good classes
+        How to format code for maximum readability
+        How to implement complete error handling without obscuring code logic
+        How to unit test and practice test-driven development
+        What “smells” and heuristics can help you identify bad code
+        ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Women Who Run with the Wolves: Myths and Stories of the Wild Woman Archetype",
+    img: "img/product-4.png",
+    price: 18,
+    author: "Clarissa Pinkola Estés Phd",
+    info:
+      "Within every woman there lives a powerful force, filled with good instincts, passionate creativity, and ageless knowing. She is the Wild Woman, who represents the instinctual nature of women. But she is an endangered species. For though the gifts of wildish nature belong to us at birth, society’s attempt to “civilize” us into rigid roles has muffled the deep, life-giving messages of our own souls.
+
+      In Women Who Run with the Wolves, Dr. Clarissa Pinkola Estés unfolds rich intercultural myths, fairy tales, folk tales, and stories, many from her own traditions, in order to help women reconnect with the fierce, healthy, visionary attributes of this instinctual nature. Through the stories and commentaries in this remarkable book, we retrieve, examine, love, and understand the Wild Woman, and hold her against our deep psyches as one who is both magic and medicine.
+
+      Dr. Estés has created a new lexicon for describing the female psyche. Fertile and life-giving, it is a psychology of women in the truest sense, a knowing of the soul.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Behold the Dreamers (Oprah's Book Club)",
+    img: "img/product-5.png",
+    price: 24,
+    author: "Imbolo Mbue",
+    info:
+      "Jende Jonga, a Cameroonian immigrant living in Harlem, has come to the United States to provide a better life for himself, his wife, Neni, and their six-year-old son. In the fall of 2007, Jende can hardly believe his luck when he lands a job as a chauffeur for Clark Edwards, a senior executive at Lehman Brothers. Clark demands punctuality, discretion, and loyalty—and Jende is eager to please. Clark’s wife, Cindy, even offers Neni temporary work at the Edwardses’ summer home in the Hamptons. With these opportunities, Jende and Neni can at last gain a foothold in America and imagine a brighter future.
+
+      However, the world of great power and privilege conceals troubling secrets, and soon Jende and Neni notice cracks in their employers’ façades.
+
+      When the financial world is rocked by the collapse of Lehman Brothers, the Jongas are desperate to keep Jende’s job—even as their marriage threatens to fall apart. As all four lives are dramatically upended, Jende and Neni are forced to make an impossible choice.
+      ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    img: "img/product-6.png",
+    price: 17,
+    author: "James Clear",
+    info:
+      "Tiny Changes, Remarkable Results
+
+      No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results.
+
+      If you're having trouble changing your habits, the problem isn't you. The problem is your system. Bad habits repeat themselves again and again not because you don't want to change, but because you have the wrong system for change. You do not rise to the level of your goals. You fall to the level of your systems. Here, you'll get a proven system that can take you to new heights.
+
+      Clear is known for his ability to distill complex topics into simple behaviors that can be easily applied to daily life and work. Here, he draws on the most proven ideas from biology, psychology, and neuroscience to create an easy-to-understand guide for making good habits inevitable and bad habits impossible. Along the way, readers will be inspired and entertained with true stories from Olympic gold medalists, award-winning artists, business leaders, life-saving physicians, and star comedians who have used the science of small habits to master their craft and vault to the top of their field.
+
+      Learn how to:
+        •  make time for new habits (even when life gets crazy);
+        •  overcome a lack of motivation and willpower;
+        •  design your environment to make success easier;
+        •  get back on track when you fall off course;
+      ...and much more.
+
+      Atomic Habits will reshape the way you think about progress and success, and give you the tools and strategies you need to transform your habits--whether you are a team looking to win a championship, an organization hoping to redefine an industry, or simply an individual who wishes to quit smoking, lose weight, reduce stress, or achieve any other goal.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Leonardo da Vinci",
+    img: "img/product-7.png",
+    price: 19.99,
+    author: "Walter Isaacson",
+    info:
+      "Based on thousands of pages from Leonardo da Vinci’s astonishing notebooks and new discoveries about his life and work, Walter Isaacson “deftly reveals an intimate Leonardo” (San Francisco Chronicle) in a narrative that connects his art to his science. He shows how Leonardo’s genius was based on skills we can improve in ourselves, such as passionate curiosity, careful observation, and an imagination so playful that it flirted with fantasy.
+
+      He produced the two most famous paintings in history, The Last Supper and the Mona Lisa. With a passion that sometimes became obsessive, he pursued innovative studies of anatomy, fossils, birds, the heart, flying machines, botany, geology, and weaponry. He explored the math of optics, showed how light rays strike the cornea, and produced illusions of changing perspectives in The Last Supper. His ability to stand at the crossroads of the humanities and the sciences, made iconic by his drawing of Vitruvian Man, made him history’s most creative genius.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "The Alchemist ",
+    img: "img/product-8.png",
+    price: 13.99,
+    author: "Paulo Coelho",
+    info:
+      "Combining magic, mysticism, wisdom and wonder into an inspiring tale of self-discovery, The Alchemist has become a modern classic, selling millions of copies around the world and transforming the lives of countless readers across generations.
+
+       Paulo Coelho's masterpiece tells the mystical story of Santiago, an Andalusian shepherd boy who yearns to travel in search of a worldly treasure. His quest will lead him to riches far different—and far more satisfying—than he ever imagined. Santiago's journey teaches us about the essential wisdom of listening to our hearts, of recognizing opportunity and learning to read the omens strewn along life's path, and, most importantly, to follow our dreams.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Brown Girl, Brownstones",
+  img: "img/product-1.png",
+  price: 8.95,
+  author: "Paule Marshall",
+  info:
+    "'An unforgettable novel, written with pride and anger, with rebellion and tears.' — Herald Tribune Book Review'Passionate, compelling . . . an impressive accomplishment.' — Saturday Review'Remarkable for its courage, its color, and its natural control.' — The New Yorker
+    Selina's mother wants to stay in Brooklyn and earn enough money to buy a brownstone row house, but her father dreams only of returning to his island home. Torn between a romantic nostalgia for the past and a driving ambition for the future, Selina also faces the everyday burdens of poverty and racism. Written by and about an African-American woman, this coming-of-age story unfolds during the Depression and World War II. Its setting — a close-knit community of immigrants from Barbados — is drawn from the author's own experience, as are the lilting accents and vivid idioms of the characters' speech. Paule Marshall's 1959 novel was among the first to portray the inner life of a young female African-American, as well as depicting the cross-cultural conflict between West Indians and American blacks. It remains a vibrant, compelling tale of self-discovery.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
