e4151cd9d27f20549ce1be3b5b07cd5cacb9aa50
Tanuka16 <tanuka.das12@gmail.com>
2020-10-19 22:25:45 -0400

    added style and link to product

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import ProductDetail from './ProductDetail';
 import Title from './Title'
-import {storeProducts} from '../bookdata';
+// import {storeProducts} from '../bookdata';
 //importing BookConsumer, since its the variable i created in our context.js
 import {BookConsumer} from '../context';
 
 class ProductList extends Component {
-  state={
-    product: storeProducts
-  }
-
+  /* state={
+  //   product: storeProducts
+   }
+   */
   render() {
     return (
       <React.Fragment>
