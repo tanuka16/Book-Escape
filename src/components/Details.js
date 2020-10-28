ff07f303da44119f8e55aafaab34907c0e40b51d
Tanuka16 <tanuka.das12@gmail.com>
2020-10-27 22:32:32 -0400

    added product 1 detail, buttons

--- src/components/Details.js
+++ src/components/Details.js
@@ -28,6 +28,46 @@ class Details extends Component {
 
             </div>
             {/* end title */}
+            {/* product info */}
+            <div className="row">
+              <div className="col-10 mx-auto col-md-6 my-3">
+                <img src={img} className="img-fluid" alt="product"/>
+              </div>
+              {/* product text */}
+              <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                <h2>model: {title}</h2>
+                <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                  made by: <span className="text-uppercase">
+                  {company}
+                  </span>
+                </h4>
+                <h4 className="text-blue">
+                  <strong>
+                    price: <span>$</span>
+                    {price}
+                  </strong>
+                </h4>
+                <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                  some info about product:
+                </p>
+                <p className="text-muted lead">{info}</p>
+                {/* buttons */}
+                <div>
+                  <Link to="/">
+                    <ButtonContainer>back to products</ButtonContainer>
+                  </Link>
+                  <ButtonContainer
+                    disabled={inCart ? true : false}
+                    onClick={() => {
+                      value.addToCart(id);
+                    }}
+                    >
+                      {inCart ? "inCart" : "add to cart"}
+                      
+                  </ButtonContainer>
+                </div>
+              </div>
+            </div>
             </div>
           )
         }}
