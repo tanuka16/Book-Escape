baafb40031d00591abd77693731536f76689e755
Tanuka16 <tanuka.das12@gmail.com>
2020-11-01 15:00:30 -0500

    tryin to fix product pg error

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -11,28 +11,42 @@ class ProductDetail extends Component {
     const {id, title, img, price, inCart} = this.props.product;
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-        <div className="card">
-          <div className="img-container p-5" onClick={()=>console.log('clicked img container')}>
 
-            <Link to="/details">
-              <img src={img} alt="product-book" className="card-img-top"/>
-            </Link>
-            <button
-              className="cart-btn"
-              disabled={inCart ? true:false}
-              onClick={()=>{console.log('added to the cart');
-              }}
-            >
+      <div className="card">
+        <ProductConsumer>
+        // write as a function - access to value handle method
+          {(value) => (
+            <div className="img-container p-5"
+                onClick={() =>
+                  value.handleDetail(id)
+                }>
 
-            {inCart ? (
-              <p className="text-capitalize mb-0" disabled>
-              {" "}
-              in Cart
-              </p>):(<i className="fas fa-cart-plus"/>
-            )}
-            </button>
+              <Link to="/details">
+                <img src={img} alt="product-book" className="card-img-top"/>
+              </Link>
+              <button
+                className="cart-btn"
+                disabled={inCart ? true:false}
+                onClick={()=>{
+                  value.addToCart(id);
+                }}
+              >
 
-          </div>
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                {" "}
+                in Cart
+                </p>):(<i className="fas fa-cart-plus"/>
+              )}
+              </button>
+
+            </div>
+
+          )}
+
+
+          </ProductConsumer>
+
           {/* card footer */}
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center md-0">{title}</p>
