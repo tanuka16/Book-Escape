dd03836ce1b3520c090aaf9bd4eeb03e135d867c
Tanuka16 <tanuka.das12@gmail.com>
2020-11-06 21:54:42 -0500

    setted up Model component and rendering

---
+++ src/components/Modal.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Modal extends Component {
+
+  render() {
+    return (
+      <div>
+        <h3>hello from model</h3>
+      </div>
+    );
+  }
+
+}
+
+export default Modal;
