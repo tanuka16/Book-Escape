fff60cd7a5442831942bdf387ee81d127376b7d8
Tanuka Das <tanuka.das12@gmail.com>
2020-12-25 22:16:21 -0500

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ class Navbar extends Component {
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
             <Link to="/" className="nav-link">
-            Products
+            Book Escape
             </Link>
           </li>
         </ul>
