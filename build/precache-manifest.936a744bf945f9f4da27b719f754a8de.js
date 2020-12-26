294633c132418b5113904515e540a0b0977b18e0
Tanuka Das <tanuka.das12@gmail.com>
2020-12-25 23:29:44 -0500

    removed few files

---
+++ build/precache-manifest.936a744bf945f9f4da27b719f754a8de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad10d369299e9ddb5a27773826bc7e54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f33bf75c425f5753875e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ee5ba12a04cbee26857e",
+    "url": "/static/css/main.b9b67833.chunk.css"
+  },
+  {
+    "revision": "f33bf75c425f5753875e",
+    "url": "/static/js/2.9669b773.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9669b773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5ba12a04cbee26857e",
+    "url": "/static/js/main.5f8d8d65.chunk.js"
+  },
+  {
+    "revision": "2256894ad484c9580512",
+    "url": "/static/js/runtime-main.10957512.js"
+  }
+]);
